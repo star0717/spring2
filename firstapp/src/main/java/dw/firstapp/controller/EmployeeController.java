448f32e9ea053e1ca39c6b60ffbd2bed0b70f1dd
Yoon Byeol <ydb0717@gmail.com>
2024-04-18 14:50:29 +0900

    init

--- firstapp/src/main/java/dw/firstapp/controller/EmployeeController.java
+++ firstapp/src/main/java/dw/firstapp/controller/EmployeeController.java
@@ -3,10 +3,7 @@ package dw.firstapp.controller;
 import dw.firstapp.model.Employee;
 import dw.firstapp.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,5 +25,21 @@ public class EmployeeController {
     @GetMapping("api/employee")
     public List<Employee> getAllEmployees(){
         return employeeService.getAllEmployees();
+    }
+
+    @GetMapping("api/employee/{id}")
+    public Employee getEmployeeById(@PathVariable long id){
+        return employeeService.getEmployeeById(id);
+    }
+
+    @PutMapping("api/employee/{id}")
+        public Employee updateEmployeeById(@PathVariable long id,
+                                           @RequestBody Employee employee){
+        return employeeService.updateEmployeeById(id, employee);
+    }
+
+    @DeleteMapping("api/employee/{id}")
+    public Employee deleteEmployeeById(@PathVariable long id){
+        return employeeService.deleteEmployeeById(id);
     }
 }
--- firstapp/src/main/java/dw/firstapp/service/EmployeeService.java
+++ firstapp/src/main/java/dw/firstapp/service/EmployeeService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EmployeeService {
@@ -20,5 +21,42 @@ public class EmployeeService {
 
     public List<Employee> getAllEmployees(){
         return employeeRepository.findAll();
+    }
+
+    public Employee getEmployeeById(long id){
+      Optional <Employee> employee = employeeRepository.findById(id);
+      // optinal로 감싸야 null이 되지않으므로 일종의 안전장치 역할로 감싸준 것
+        if (employee.isEmpty()){
+            // 예외처리
+            return null;
+        }else {
+            return employee.get();
+        }
+    }
+
+    public Employee updateEmployeeById(long id, Employee employee){
+        // ID로 해당 데이터 찾기
+        Optional <Employee> employee1 = employeeRepository.findById(id);
+        if (employee1.isPresent()){
+            // 데이터 업데이트
+            employee1.get().setEmail(employee.getEmail());
+            employee1.get().setFirstName(employee.getFirstName());
+            employee1.get().setLastName(employee.getLastName());
+            // 실제 DB에 저장하기
+            employeeRepository.save(employee1.get());
+            return employee1.get();
+        }else{
+            return null;
+        }
+    }
+
+    public Employee deleteEmployeeById(long id){
+        Optional<Employee> employee = employeeRepository.findById(id);
+        if (employee.isPresent()){
+            employeeRepository.deleteById(id);
+            return employee.get();
+        }else {
+            return null;
+        }
     }
 }
