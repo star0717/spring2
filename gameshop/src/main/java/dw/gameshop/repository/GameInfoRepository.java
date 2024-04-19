6c3a1c7228f303c5f2947ffd1cffbb841a6eb471
Yoon Byeol <ydb0717@gmail.com>
2024-04-19 15:15:26 +0900

    init

--- gameshop/src/main/java/dw/gameshop/repository/GameInfoRepository.java
+++ gameshop/src/main/java/dw/gameshop/repository/GameInfoRepository.java
@@ -2,8 +2,10 @@ package dw.gameshop.repository;
 
 import dw.gameshop.model.Game;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
 
 import java.util.Optional;
+
 
 public interface GameInfoRepository extends JpaRepository<Game, String> {
     Optional<Game> findById(long id);
