d4d747850499275c131e28b9c9ceab37928b83df
Yoon Byeol <ydb0717@gmail.com>
2024-04-19 17:24:40 +0900

    Update PurchaseController.java

--- gameshop/src/main/java/dw/gameshop/controller/PurchaseController.java
+++ gameshop/src/main/java/dw/gameshop/controller/PurchaseController.java
@@ -1,4 +1,29 @@
 package dw.gameshop.controller;
 
+import dw.gameshop.model.Purchase;
+import dw.gameshop.service.PurchaseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class PurchaseController {
+    @Autowired
+    PurchaseService purchaseService;
+
+    @PostMapping("/products/purchase")
+    public Purchase savePurchase(@RequestBody Purchase purchase){
+        return purchaseService.savePurchase(purchase);
+    }
+
+    @GetMapping("/products/purchase")
+    public List<Purchase> getAllpurchases(){
+        return purchaseService.getAllpurchases();
+    }
+
+    @GetMapping("/products/purchase/{userId}")
+    public List<Purchase> getPurchaseListById(@PathVariable String userId) {
+        return purchaseService.getPurchaseListByUser(userId);
+    }
 }
